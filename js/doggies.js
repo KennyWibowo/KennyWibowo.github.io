dec13c31ac660e1a5cb5496b3928e7602756549c
Kenny Wibowo <kewibowo@ucsd.edu>
2016-11-29 18:01:36 -0800

    Hide my face when doggies are present

--- js/doggies.js
+++ js/doggies.js
@@ -1,36 +1,88 @@
-var dogtext = document.getElementById("dogtext");
-var header = document.getElementById("header");
+(function($) {
 
-var image = new Image();
+    // non-jQuery vanilla functionality of easter egg.
+    var noJQ = function() {
+        var dogtext = document.getElementById("dogtext");
+        var header = document.getElementById("header");
+        var me = document.getElementById("me");
 
-var doggified = false;
-var dogswitch = true;
+        var image = new Image();
 
-image.onload = function() {
-	var prev_bkg = header.style.backgroundImage;
-	dogtext.onclick = function() {
+        var doggified = false;
+        var dogswitch = true;
 
-		// Switch between default background and original background
-		if(!doggified) {
-			prev_bkg = header.style.backgroundImage;
-			header.style.backgroundImage = 'url("https://wibow.io/res/doggies.png")';
-			
-			// Switch between Bella and Chewie
-			if(dogswitch) {
-				header.style.backgroundPosition = "left top";
-			} else {
-				header.style.backgroundPosition = "right top";
-			}
+        image.onload = function() {
+            var prev_bkg = header.style.backgroundImage;
+            dogtext.onclick = function() {
 
-			dogswitch = !dogswitch;
-		} else {
-			header.style.backgroundImage = prev_bkg;
-			header.style.backgroundPosition = "center top";
-		}
+                // Switch between default background and original background
+                if(!doggified) {
+                    prev_bkg = header.style.backgroundImage;
+                    header.style.backgroundImage = 'url("https://wibow.io/res/doggies.png")';
 
-		doggified = !doggified;
-	}
-}
+                    me.style.visibility = "hidden";
+                    
+                    // Switch between Bella and Chewie
+                    if(dogswitch) {
+                        header.style.backgroundPosition = "left top";
+                    } else {
+                        header.style.backgroundPosition = "right top";
+                    }
 
-// Prefetch the image
-image.src = "https://wibow.io/res/doggies.png"
+                    dogswitch = !dogswitch;
+                } else {
+                    me.style.visibility = "";
+                    header.style.backgroundImage = prev_bkg;
+                    header.style.backgroundPosition = "center top";
+                }
+
+                doggified = !doggified;
+            }
+        }
+
+        // Prefetch the image
+        image.src = "https://wibow.io/res/doggies.png";
+    }
+
+    // TODO: animate using animate.css and jQuery
+    var yesJQ = function($) {
+        if(!$) {
+            return;
+        }
+
+        var dogtext = $('#dogtext');
+        var header = $('#header');
+    }
+
+    // Cache the previous onload and run any passed in one.
+    var addOnloadEvent = function(event) {
+        if(typeof window.onload != 'function') { 
+            window.onload = event;  // If first in chain, just set onload as function itself
+        } else {
+            // Execute previous onload event
+            var prev = window.onload;
+
+            // Can chain caches within caches for multiple onload functions.
+            window.onload = function() {
+                if(prev) {
+                    prev();
+                }
+                
+                event();
+            };
+        }
+    }
+
+
+    addOnloadEvent(function() {
+        // Execute based off of whether we have jQuery.
+        /*if (window.jQuery) {  
+            yesJQ(window.jQuery);
+        } else {
+            noJQ();
+        }*/
+
+        noJQ();
+    });
+
+})();
